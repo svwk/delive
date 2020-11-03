4a7b41aa8f15fc48369ac8cdcfd182c7d118f693
svs <sv.ework@gmail.com>
2020-11-03 19:50:48 +0300

    commit25

--- db_create.py
+++ db_create.py
@@ -1,2 +1,35 @@
-from delive import db
+from delive import db, config, Dish, Category
+db.drop_all()
 db.create_all()
+
+try:
+    with open(f'{config.current_path}/data/source_data.csv', 'r') as f:
+        content = f.read()
+except FileNotFoundError:
+    content = None
+if content:
+    lst = content.split('\n')
+    if len(lst) > 1:
+        for value in lst[1:]:
+            items = value.split('|')
+            if items and len(items) > 5:
+                dish = Dish(id=int(items[0]), title=items[1], price=int(items[2]), description=items[3],
+                            picture=items[4], category_id=int(items[5]))
+                db.session.add(dish)
+
+try:
+    with open(f'{config.current_path}/data/category.csv', 'r') as f:
+        content = f.read()
+except FileNotFoundError:
+    content = None
+
+if content and len(content) > 1:
+    lst = content.split('\n')
+    if len(lst) > 1:
+        for value in lst[1:]:
+            items = value.split('|')
+            if items and len(items) > 1:
+                cat = Category(id=int(items[0]), title=items[1])
+                db.session.add(cat)
+
+db.session.commit()
--- delive/__init__.py
+++ delive/__init__.py
@@ -5,6 +5,11 @@ from delive.models import db, migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.config.update(
+    SESSION_COOKIE_SECURE=False,
+    SESSION_COOKIE_HTTPONLY=True,
+    SESSION_COOKIE_SAMESITE='Lax',
+)
 
 db.init_app(app)
 migrate.init_app(app, db, f'{config.current_path}/migrations')
